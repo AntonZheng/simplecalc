1a7f8675d1a02ae723f1db3d3ae67858e4cc8cf3
antonzheng <anton.zheng@gmail.com>
2018-01-17 20:10:14 -0800

    finished calculator

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,51 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        let eqLength = args.count
+        switch args[eqLength - 1] {
+        case "count":
+            return eqLength - 1
+        case "avg":
+            if(eqLength == 1) {
+                return 0
+            }
+            var sum = 0
+            for i in 0...(eqLength - 1) {
+                if(args[i] != "avg") {
+                    sum += Int(args[i])!
+                }
+            }
+            return sum / (eqLength - 1)
+        case "fact":
+            if(eqLength == 1) {
+                return 0
+            } else {
+                let num = Int(args[0])!
+                return fact(num)
+            }
+        default:
+            switch args[1]{
+            case "+":
+                return Int(args[0])! + Int(args[2])!
+            case "-":
+                return Int(args[0])! - Int(args[2])!
+            case "*":
+                return Int(args[0])! * Int(args[2])!
+            case "/":
+                return Int(args[0])! / Int(args[2])!
+            case "%":
+                return Int(args[0])!-((Int(args[0])! / Int(args[2])!) * Int(args[2])!)
+            default:
+                return 0
+            }
+        }
+    }
+    
+    public func fact(_ num: Int) -> Int {
+        if(num == 1 || num == 0) {
+            return 1;
+        }
+        return fact(num - 1) * num
     }
     
     public func calculate(_ arg: String) -> Int {
